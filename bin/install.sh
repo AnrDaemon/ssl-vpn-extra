97666277390b77c71cfdfa2e6dbd079e4a9677f3
Andrey Repin <a.repin@extracode.net>
2019-09-13 15:33:38 +0300

    Installation fixes and an updated vpnc-script

--- bin/install.sh
+++ bin/install.sh
@@ -1,18 +1,28 @@
-#!/bin/sh -x
+#!/bin/sh
 
 BASE=/opt/ssl-vpn-extra
+CONF=/etc/vpnc
 _SRC="$( dirname "$( dirname "$( readlink -f "$0" )" )" )"
 
 set -e
 
 groupadd --system --force -- ssl-vpn
 
-install --verbose -D --mode=0755 "--target-directory=$BASE/bin" -- "$_SRC/bin/"*
-install --verbose -D --mode=0755 "--target-directory=$BASE/hooks" -- "$_SRC/hooks/"*
-install --verbose -D --mode=0644 "--target-directory=$BASE/sudoers" -- "$_SRC/sudoers/"*
-install --verbose --mode=0755 --directory -- /etc/vpnc/{attempt-reconnect,connect,disconnect,post-connect,post-disconnect,pre-init,reconnect}.d
+install --verbose -D --mode=0755 "--target-directory=$BASE/bin" -- "$_SRC/bin"/*
+install --verbose -D --mode=0755 "--target-directory=$BASE/hooks" -- "$_SRC/hooks"/*
+install --verbose -D --mode=0644 "--target-directory=$BASE/sudoers" -- "$_SRC/sudoers"/*
+install --verbose --mode=0755 --directory -- \
+  "$CONF/pre-init.d" \
+  "$CONF/connect.d" \
+  "$CONF/post-connect.d" \
+  "$CONF/disconnect.d" \
+  "$CONF/post-disconnect.d" \
+  "$CONF/attempt-reconnect.d" \
+  "$CONF/reconnect.d"
 
-visudo -cf "$_SRC/sudoers/ssl-vpn" && install --verbose -D "--target-directory=/etc/sudoers.d" -- "$_SRC/sudoers/ssl-vpn"
+visudo -cf "$BASE/sudoers/ssl-vpn" && install --verbose "--no-target-directory" -- "$BASE/sudoers/ssl-vpn" "/etc/sudoers.d/ssl-vpn"
+
+install --verbose --mode=0755 "--no-target-directory" -- "$BASE/hooks/vpnc-script.2019-06-06" "$CONF/vpnc-script"
 
 _t="/usr/local/bin/ssl-vpn"
 [ -f "$_t" -a -L "$_t" ] || ln -sT "$BASE/bin/ssl-vpn" "$_t"
@@ -20,5 +30,5 @@ _t="/usr/local/bin/ssl-vpn"
 _t="/usr/local/bin/ssl-vpn-disconnect"
 [ -f "$_t" -a -L "$_t" ] || ln -sT "$BASE/bin/ssl-vpn-disconnect" "$_t"
 
-_t="/etc/vpnc/post-connect.d/x5-routes"
+_t="$CONF/post-connect.d/x5-routes"
 [ -f "$_t" -a -L "$_t" ] || ln -sT "$BASE/hooks/x5-routes.sh" "$_t"
---
+++ hooks/README.hooks.md
@@ -0,0 +1,4 @@
+# `vpnc-script` updates
+
+If issues arise, grab an updated script at the
+http://git.infradead.org/users/dwmw2/vpnc-scripts.git/blob_plain/HEAD:/vpnc-script
