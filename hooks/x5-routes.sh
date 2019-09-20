f4e03c95627fd592678b9ada493f650b8c961f27
Andrey Repin <a.repin@extracode.net>
2019-09-20 20:09:07 +0300

    Added x5 wiki route
    Added Windows rerouting script.

--- hooks/x5-routes.sh
+++ hooks/x5-routes.sh
@@ -3,10 +3,11 @@
 [ "$NETDEV" ] || exit
 [ "$CISCO_DEF_DOMAIN" = "x5.ru" ] || exit
 
+ip route replace 192.168.32.0/24 dev $NETDEV
 ip route replace 192.168.129.0/24 dev $NETDEV
+ip route replace 192.168.181.0/24 dev $NETDEV
 ip route replace 192.168.191.0/24 dev $NETDEV
 ip route replace 192.168.239.0/24 dev $NETDEV
-ip route replace 192.168.32.0/24 dev $NETDEV
 
 ip route delete 192.168.0.0/16 dev $NETDEV
 ip route delete 172.16.0.0/12 dev $NETDEV
---
+++ support/reroute-x5.cmd
@@ -0,0 +1,19 @@
+@ECHO OFF
+
+route /p ADD 85.115.249.1 %~1
+route /p ADD 85.115.249.2 %~1
+route /p ADD 85.115.249.3 %~1
+route /p ADD 85.115.249.4 %~1
+route /p ADD 91.206.100.0/23 %~1
+route /p ADD 193.232.108.0/24 %~1
+REM route /p ADD 195.42.96.0/23 %~1
+
+route /p ADD 192.168.66.131 %~1
+route /p ADD 192.168.129.5 %~1
+route /p ADD 192.168.32.0/24 %~1
+route /p ADD 192.168.129.0/24 %~1
+route /p ADD 192.168.181.0/24 %~1
+route /p ADD 192.168.191.0/24 %~1
+route /p ADD 192.168.239.0/24 %~1
+
+route /p ADD 172.18.236.0/22 %~1
